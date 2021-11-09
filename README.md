567a5a2bc16f76cb176c965be7e7c1a9aa20bd17
PureGero <puregero@gmail.com>
2021-11-09 17:57:53 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# graduation-video
+
+Graduation video creator
+
+## Building
+
+Build the code with maven:
+
+```mvn```
+
+The jar file can then be found under `target/`
---
+++ src/main/java/me/puregero/graduationvideo/GraduationVideo.java
@@ -0,0 +1,8 @@
+package me.puregero.graduationvideo;
+
+public class GraduationVideo {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
